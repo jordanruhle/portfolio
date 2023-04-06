b16eedf9a83b3143890444d142759aa6db84bde1
Jordan Ruhle <jordanruhle@gmail.com>
2023-04-06 11:41:19 -0700

    made initial components

---
+++ client/src/components/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      {/* Your Portfolio content here */}
+    </section>
+  );
+};
+
+export default Portfolio;
