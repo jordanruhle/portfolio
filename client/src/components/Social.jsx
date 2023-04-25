9eefbd664cab6830b980819bd07ffac9576417ea
Jordan Ruhle <jordanruhle@gmail.com>
2023-04-25 10:29:43 -0700

    updated linkedin url

--- client/src/components/Social.jsx
+++ client/src/components/Social.jsx
@@ -10,7 +10,7 @@ const SocialLinks = () => {
       <p className='text-white text-xl'>Jordan Ruhle</p>
       <ul className='flex justify-center items-center gap-8'>
         <li><a href="https://github.com/jordanruhle"><img src={githubLogo} alt="gmail" className='h-14'/></a></li>
-        <li><a href="https://www.linkedin.com/in/jordan-ruhle-76317198/"><img src={linkedInLogo} alt="gmail"  className='h-14'/></a></li>
+        <li><a href="https://www.linkedin.com/in/jordan-ruhle/"><img src={linkedInLogo} alt="gmail"  className='h-14'/></a></li>
         <li><a href="mailto:jordanruhle@gmail.com"><img src={gmailLogo} alt="gmail" className='h-16'/></a></li>
       </ul>
       <p className='text-white text-xl'>jordanruhle@gmail.com</p>
